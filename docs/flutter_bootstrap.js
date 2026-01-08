ab6e7b70cf551b2a1bab460b9756613b6aa5c881
idzzzham11 <mohdidzham1102@gmail.com>
2026-01-08 13:14:24 +0800

    Deploy Flutter web build to /docs

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2794427668"
+    serviceWorkerVersion: "1454384512"
   }
 });
